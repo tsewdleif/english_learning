f00bca5c2d257a96d8706a0d611b32e128080e53
WestField <tsewdleif@gmail.com>
2020-05-12 23:24:57 +0800

    project init

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # english_learning
 listening, words, writing
+
+lemmatization-en.txt来源于https://github.com/michmech/lemmatization-lists
---
+++ listening/word.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# @Time : 2020/5/12 0012 下午 23:02
+# @Author : West Field
+# @File : word.py
+
+
